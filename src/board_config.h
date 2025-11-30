a28d7b718c864a1f2030f6d9a1b9645bbf9c3199
diglet48 <diglet48@gmail.com>
2025-11-30 11:02:01 +0100

    fix gpio port numbers

--- src/board_config.h
+++ src/board_config.h
@@ -6,15 +6,15 @@
 #if defined(BOARD_FOCSTIM_V4_0)
 #define FOC_I2C_SCL_GPIO (GPIO_NUM_7)
 #define FOC_I2C_SDA_GPIO (GPIO_NUM_1)
-#define FOC_UART_RX_GPIO (GPIO_NUM_33)
-#define FOC_UART_TX_GPIO (GPIO_NUM_34)
+#define FOC_UART_RX_GPIO (GPIO_NUM_17)
+#define FOC_UART_TX_GPIO (GPIO_NUM_18)  // changed in V4.1 due to presence of startup glitches on this pin
 
 
 #elif defined(BOARD_FOCSTIM_V4_1)
 #define FOC_I2C_SCL_GPIO (GPIO_NUM_38)
 #define FOC_I2C_SDA_GPIO (GPIO_NUM_37)
-#define FOC_UART_RX_GPIO (GPIO_NUM_17)
-#define FOC_UART_TX_GPIO (GPIO_NUM_18)  // changed in V4.1 due to presence of startup glitches on this pin
+#define FOC_UART_RX_GPIO (GPIO_NUM_33)
+#define FOC_UART_TX_GPIO (GPIO_NUM_34)
 
 
 #else
